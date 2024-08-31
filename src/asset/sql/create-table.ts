ae6a327710895093862c00bc0216e4bbdaecfc32
thachtho <buithanhtho15ig@gmail.com>
2024-08-31 12:15:02 +0700

    remove init db

--- src/asset/sql/create-table.ts
+++
@@ -1,9 +0,0 @@
-export const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        email VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        nickname VARCHAR(255) NOT NULL,
-        role INT NOT NULL
-    )
-`;
